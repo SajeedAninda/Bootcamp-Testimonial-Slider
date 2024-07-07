7d7c09fd95764ce2c73605838ecf416299288176
SajeedAninda <sajaninda@gmail.com>
2024-07-07 18:45:43 +0600

    Added text and images

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import "./slider.css";
+import tanyaImg from "../assets/image-tanya.jpg";
 
 const Slider = () => {
+    let testimonials = [
+        {
+            name: 'Tanya Sinclair',
+            position: 'UX Engineer',
+            testimonial: 'I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.',
+            image: tanyaImg
+        },
+    ]
+
     return (
         <div className='h-screen w-full  flex justify-center items-center '>
-            <div className='h-[90%] w-[90%] mx-auto sliderDiv'>
+            <div className='h-[90%] w-[80%] mx-auto sliderDiv flex items-center justify-between'>
+                <div className="quoteDiv w-[50%]">
+                    <h1 className='text-[32px] font-light text-[#202046]'>
+                        {testimonials[0].testimonial}
+                    </h1>
 
+                    <div className='mt-5 flex gap-4'>
+                        <p className='text-[#202046] text-[18px] font-bold'>
+                            {testimonials[0].name}
+                        </p>
+                        <p className='text-[#BABACF] text-[18px] font-semibold'>
+                            {testimonials[0].position}
+                        </p>
+                    </div>
+                </div>
+                <div className="imgDiv w-[50%] flex justify-center">
+                    <img className='w-[450px]' src={testimonials[0].image} alt="" />
+                </div>
             </div>
         </div>
     );
